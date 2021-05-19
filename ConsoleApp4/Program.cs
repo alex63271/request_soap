8dfeae3fd3c187e0e70556a659dc6a68a0706864
Алексей Новосельцев <alex6327@yandex.ru>
2021-05-19 17:44:35 +0300

    убрал лишние классы

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -14,30 +14,7 @@ namespace svs_json
 {
 
 
-    public class Class1
-    {
-        public object Message { get; set; }
-        public bool Result { get; set; }
-        public string SignerCertificate { get; set; }
-        public Signercertificateinfo SignerCertificateInfo { get; set; }
-        public Signatureinfo SignatureInfo { get; set; }
-        public object AdditionalCertificateResult { get; set; }
-    }
-
-    public class Signercertificateinfo
-    {
-        public string SubjectName { get; set; }
-        public string IssuerName { get; set; }
-        public DateTime NotBefore { get; set; }
-        public DateTime NotAfter { get; set; }
-        public string SerialNumber { get; set; }
-        public string Thumbprint { get; set; }
-    }
-
-    public class Signatureinfo
-    {
-        public string CAdESType { get; set; }
-    }
+   
 
 
     class Program
@@ -45,17 +22,18 @@ namespace svs_json
         static async void  PostRequestAsync()
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://ruzditest.eisnot.ru:8280/services/ruzdiUploadNotificationPackageService_v1_1");
-          request.Method = "POST";
-            request.ContentType = "text/xml";
-            request.Headers.Add("SOAPAction", "http://fciit.ru/eis2/ruzdi/uploadNotificationPackageService");
-            XmlDocument xDoc = new XmlDocument();
-            xDoc.Load("C://123.xml");
-            string data1=xDoc.OuterXml;
-            Console.WriteLine(data1);
             
-            byte[] Array = Encoding.UTF8.GetBytes(data1);
+                request.Method = "POST";
+                request.ContentType = "text/xml";
+                request.Headers.Add("SOAPAction", "http://fciit.ru/eis2/ruzdi/uploadNotificationPackageService");
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load("C://123.xml");
+                string data1 = xDoc.OuterXml;
+                Console.WriteLine(data1);
 
-            request.ContentLength = Array.Length;
+                byte[] Array = Encoding.UTF8.GetBytes(data1);
+
+                request.ContentLength = Array.Length;
 
 
             using (Stream writer = request.GetRequestStream())
@@ -64,38 +42,32 @@ namespace svs_json
                 writer.Write(Array, 0, Array.Length);
 
 
-                Console.WriteLine("Запрос создан");
-
-                HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
-
-
-
-
-                using (Stream stream = response.GetResponseStream())
-                {
-                    using (StreamReader reader = new StreamReader(stream))
-                    {
-
-                        string q = reader.ReadToEnd();
-                        Console.WriteLine(q);
-
-                        XmlDocument xDocout = new XmlDocument();
-                        xDocout.LoadXml(q);
-                        xDocout.Save("1234.xml");
-                        //Class1[] otvet = JsonSerializer.Deserialize<Class1[]>(q);
-                        //Console.WriteLine("Message:");
-                        //Console.WriteLine(otvet[0].Message);
-                        //Console.WriteLine("SubjectName:");
-                        //Console.WriteLine(otvet[0].SignerCertificateInfo.SubjectName);
-                        //Console.WriteLine("Thumbprint:");
-                        //Console.WriteLine(otvet[0].SignerCertificateInfo.Thumbprint);
-
-
-                    }
-                }
-
+               
             }
+                    HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            Console.WriteLine("Запрос создан");
 
+
+
+            using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+
+                            string q = reader.ReadToEnd();
+                            Console.WriteLine(q);
+
+                            XmlDocument xDocout = new XmlDocument();
+                  
+                            xDocout.LoadXml(q);
+                            xDocout.Save("1234.xml");
+                       
+
+                        }
+                    }
+
+                
+            
 
 
 
